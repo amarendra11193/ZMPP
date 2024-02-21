8577bb46401c79429816b31acc5c8720315cc81d
amarendra11193 <55204064+amarendra11193@users.noreply.github.com>
2024-02-22 00:39:20 +0530

    Update zmpp_1.8.py
    added comments

--- zmpp_1.8.py
+++ zmpp_1.8.py
@@ -1,3 +1,6 @@
+#Write a Python program to exchange the value of two variables.
+
+#Using 3rd variable
 a = 2
 b = 5
 c = a
@@ -5,6 +8,8 @@ a = b
 b = c
 print ('value of a = ',a)
 print ('value of b = ',b)
+
+#Using only two variables
 a = a+b
 b = a-b
 a = a-b
