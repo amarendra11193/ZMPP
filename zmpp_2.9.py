e96b2caf816e3695a39133473d6d1ecfe82fb5da
amarendra11193 <55204064+amarendra11193@users.noreply.github.com>
2024-02-23 01:21:44 +0530

    Update
    Book_ZMPP Chapter 2: Q9 in Exercises

---
+++ zmpp_2.9.py
@@ -0,0 +1,9 @@
+# Write a program to calculate the inverse of entered number.
+
+def inverse():
+	num = int(input('Enter a number: \n'))
+	inNum = 1/num
+	print ('Inverse of the provides number is : ', inNum)
+	return inNum
+
+inverse()
