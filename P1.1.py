93b7c12620e34a94a7d5b735c0a5f3a21eee19a5
amarendra11193 <55204064+amarendra11193@users.noreply.github.com>
2024-02-23 20:04:24 +0530

    Delete P1.1.py
    This code is part of other repository and is not required here.

--- P1.1.py
+++
@@ -1,9 +0,0 @@
-def grossSalary ():
-    bs = float(input('Enter the base Salary: \n'))
-    da = bs*0.4
-    hra = bs*0.2
-    gs = bs + da + hra
-    print ('Gross Salary is', gs)
-    return gs
-
-grossSalary()
