356079a95b8167a27428d80eaa19a8d90c167ffe
Amrendra Kumar <amarendra11193@gmail.com>
2024-03-06 18:25:15 +0530

    Book_ZMPP Chapter 6: Q3 in Exercises

---
+++ zmpp_6.3.py
@@ -0,0 +1,28 @@
+# Write a python program to find the repeated items of a tuple.
+
+def new_tuple(n):
+    lst = [ ]
+    for i in range(0, n):
+        a = input()
+        lst.append(a)
+        tup = tuple(lst)
+    return tup
+
+def repeat_item(tup):
+    i = 1
+    while (tup[i:]):
+        j = 0
+        while (tup[j:]):
+            if (i != j):
+                if (tup[i] == tup[j]):
+                    print(tup[j])
+                else:
+                    pass
+            else:
+                pass
+            j += 1
+        i += 1
+
+# Example:
+tup = (1,1,3,3,4,4,5,6)
+repeat_item(tup)
