18915c56feed68dba35e951a62c6c975b2900b8c
Amrendra Kumar <amarendra11193@gmail.com>
2024-03-12 15:28:50 +0530

    Book_ZMPP Chapter 7: Q8 in Exercises

---
+++ arithmatic.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    x = a+b
+    print('The sum of the two numbers is: ',x)
+
+def sub(a, b):
+    x = a-b
+    print('The difference of the two numbers is: ',x)
+
+def mul(a, b):
+    x = a*b
+    print('The product of the two numbers is: ',x)
+
+def div(a, b):
+    x = a/b
+    print('The divison of the two numbers is: ',x)
---
+++ zmpp_7.8.py
@@ -0,0 +1,5 @@
+# Write a program that performs basic calculator functions using modules & packages.
+
+from arithmatic import add
+
+add(3,5)
